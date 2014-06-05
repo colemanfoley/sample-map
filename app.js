b87abd15bbca6e0454d9276674bc6829e2a0dc5f
Coleman Foley <colemanfoley@gmail.com>
2014-06-05 14:32:18 -0700

    Made a div for the graph.

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ R.ready(function() {
 var width = 960,
     height = 500
 
-var svg = d3.select("body").append("svg")
+var svg = d3.select("#graphContainer").append("svg")
     .attr("width", width)
     .attr("height", height);
 
